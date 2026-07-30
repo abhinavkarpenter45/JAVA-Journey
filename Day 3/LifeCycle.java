b86b1d2d093ad553651ab71042dbb022f786faa4
Abhinav Karpenter <abhinavkarpenter1@gmail.com>
2026-07-30 20:14:44 +0530

    Day 3:Learn concept about Life Cycle and Variables

---
+++ Day 3/LifeCycle.java
@@ -0,0 +1,26 @@
+/*
+
+Hello.java
+      │
+      ▼
+javac (Compiler)
+      │
+      ▼
+Hello.class (Bytecode)
+      │
+      ▼
+     JVM
+      │
+      ▼
+ Class Loader
+      │
+      ▼
+Bytecode Verifier
+      │
+      ▼
+Execution Engine (Interpreter + JIT)
+      │
+      ▼
+    Output
+    
+*/
